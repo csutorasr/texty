1f047e6e6968d01335d83a3e9431a56c137d7f91
Csutorás Robin <csutorasr@hotmail.com>
2017-02-02 03:48:32 +0100

    applier tester added

--- example/example.js
+++ example/example.js
@@ -2,4 +2,8 @@ var editor = texty.init(document.getElementById('editor'));
 editor.onSelectionChanged(function() {
     console.log(editor.activeAppliers);
 });
+editor.addApplier('bold',{
+    elementTagName: 'strong'
+});
+editor.init();
 console.log(editor);
