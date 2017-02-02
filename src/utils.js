6a5abf66cf757475e54dcbc574dfac38d0c84f09
Csutorás Robin <csutorasr@hotmail.com>
2017-02-02 01:26:17 +0100

    nodeName is faster

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ function LoadUtils(){
     texty.utils.blockTagNames = ['H1','H2','H3','H4'];
     texty.utils.filterBlockNodes = function(nodes) {
         return nodes.filter(function(node){
-            return (node.tagName) && texty.utils.blockTagNames.indexOf(node.tagName) !== -1;
+            return texty.utils.blockTagNames.indexOf(node.nodeName) !== -1;
         });
     };
 }
