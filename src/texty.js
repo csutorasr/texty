8922a4a972cab7b5f309ea7875098c0262f929bf
Csutorás Robin <csutorasr@hotmail.com>
2017-02-02 03:05:55 +0100

    undo redo works with keyboard shortcuts

--- src/texty.js
+++ src/texty.js
@@ -2,6 +2,19 @@ function Texty(element) {
     var _element = element;
     var appliers = {};
     var _this = this;
+    var versions = [], olderVersions = [], versionFallbackNeeded = false;
+    var currentVersion = 0;
+    var versionfallback = function () {
+        if (versionFallbackNeeded) {
+            versions = olderVersions.concat([]);
+            if (currentVersion > versions.length) {
+                currentVersion = versions.length;
+            }
+            else {
+                currentVersion--;
+            }
+        }
+    };
     var getSelectedNodes = function () {
         var sel = rangy.getSelection();
         var selectedNodes = [];
@@ -50,11 +63,46 @@ function Texty(element) {
             _this.selectionChanged();
         }
     };
+    _this.onChange = function (e) {
+        if (_this.isRedoable()) {
+            versions = versions.filter(function (value, index) {
+                return index < currentVersion;
+            });
+        }
+        else {
+            olderVersions = versions;
+        }
+        versions = versions.concat([]);
+        currentVersion = versions.push(_element.innerHTML);
+        versionFallbackNeeded = true;
+    };
+    _this.redo = function () {
+        if (_this.isRedoable()) {
+            currentVersion++;
+            _element.innerHTML = versions[currentVersion - 1];
+            versionFallbackNeeded = false;
+        }
+    };
+    _this.isRedoable = function () {
+        return (currentVersion !== versions.length);
+    };
+    _this.undo = function () {
+        if (_this.isUndoable()) {
+            currentVersion--;
+            _element.innerHTML = versions[currentVersion - 1];
+            versionFallbackNeeded = false;
+        }
+    };
+    _this.isUndoable = function () {
+        return (currentVersion !== 1);
+    };
     _this.onSelectionChanged = function (callback) {
         _this.selectionChanged = callback;
     };
     _this.parseInput = function (input) {
         _element.innerHTML = input;
+        versionFallbackNeeded = false;
+        _this.onChange();
     };
     _this.getOutput = function () {
         return _element.innerHTML;
@@ -72,6 +120,23 @@ function Texty(element) {
             }
             blockNode.parentNode.replaceChild(newNode, blockNode);
         }
+        _this.onChange();
+    };
+    _this.keyboardShortcuts = function (e) {
+        var evtobj = window.event ? event : e;
+        if (evtobj.ctrlKey) {
+            if (evtobj.keyCode == 90) {
+                versionfallback();
+                _this.undo();
+            }
+            if (evtobj.keyCode == 89) {
+                versionfallback();
+                _this.redo();
+            }
+            if (evtobj.keyCode == 86) {
+                // TODO: ctrl+V
+            }
+        }
     };
     _this.isImageSelected = false;
     _this.isLinkSelected = false;
@@ -82,13 +147,18 @@ Texty.prototype.init = function () {
     var element = this.element();
     element.setAttribute('contenteditable', 'true');
     element.addEventListener('keyup', this.onSelectionEnds);
+    element.addEventListener('input', this.onChange);
     document.addEventListener('mouseup', this.onSelectionEnds);
+    element.addEventListener('keyup', this.keyboardShortcuts);
+    this.onChange();
 };
 Texty.prototype.destory = function () {
     var element = this.element();
     element.removeAttribute('contenteditable');
     element.removeEventListener('keyup', this.onSelectionEnds);
+    element.removeEventListener('input', this.onChange);
     document.removeEventListener('mouseup', this.onSelectionEnds);
+    document.removeEventListener('keyup', this.keyboardShortcuts);
 };
 function LoadTexty() {
     rangy.init();
