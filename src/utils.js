eb3c35764be2ce5451ae54dc0515616cefbfc3ae
Csutorás Robin <csutorasr@hotmail.com>
2017-02-02 01:35:14 +0100

    blocktagnames added

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
-function LoadUtils(){
-    texty.utils.blockTagNames = ['H1','H2','H3','H4'];
-    texty.utils.filterBlockNodes = function(nodes) {
-        return nodes.filter(function(node){
+function LoadUtils() {
+    texty.utils.blockTagNames = ["ADDRESS", "ARTICLE", "ASIDE", "BLOCKQUOTE", "CANVAS", "DD", "DIV", "DL", "FIELDSET", "FIGCAPTION", "FIGURE", "H1", "H2", "H3", "H4", "H5", "H6", "FOOTER", "FORM", "HEADER", "HGROUP", "HR", "LI", "MAIN", "NAV", "NOSCRIPT", "OL", "OUTPUT", "P", "PRE", "SECTION", "TABLE", "TFOOT", "UL", "VIDEO"];
+    texty.utils.filterBlockNodes = function (nodes) {
+        return nodes.filter(function (node) {
             return texty.utils.blockTagNames.indexOf(node.nodeName) !== -1;
         });
     };
